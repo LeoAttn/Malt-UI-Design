41cf8a04d309e733f14d81781f88462c44d2021a
NokiDev <noki.dev@gmail.com>
2018-10-19 15:43:57 +0200

    Added advanced search. + routes +  ayoutmodifs

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/advanced_search', function (req, res, next) {
+
+    res.render('advanced_search', {cats: categories});
+});
+router.get('/fav', function (req, res, next) {
+    res.render('fav', {title: 'Express'});
+});
+router.get('/missions', function (req, res, next) {
+    res.render('missions', {title: 'Express'});
+});
+router.get('/messages', function (req, res, next) {
+    res.render('messages', {title: 'Express'});
+});
+router.get('/message:id', function (req, res, next) {
+    res.render('message', {title: 'Express'});
+});
+router.get('/profile', function (req, res, next) {
+    res.render('profile', {title: 'Express'});
 });
 
 module.exports = router;
