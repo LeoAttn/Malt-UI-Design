69afd578d8a4dc81c8976c9c3fb185dc1d717fbd
LeoAttn <leo.massonnat@hotmail.fr>
2018-10-19 15:49:44 +0200

    fix layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
+router.get('/messages', function(req, res, next) {
+    res.render('messages', { title: 'Messages' });
+});
 module.exports = router;
